ef472cb80346841d6b74b93f105543637a6051da
German Mendoza <german.mendoza.187@gmail.com>
2015-08-26 15:28:33 -0430

    changes form login

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -1,3 +1,4 @@
+@include('frontend.forms.login')
 @extends('frontend.app')
 @section('content')
   <div class="container-fluid title">
@@ -20,37 +21,7 @@
                       @endforeach
                   </div>
               @endif
-              {!! Form::open(['route'=>'login', 'role' => 'form', 'name' => 'loginForm', 'id'=>'loginForm', 'data-toggle' => 'validator', 'class' => 'form-horizontal' ]) !!}
-                <div class="form-group has-feedback">
-                    <label for="email" class="col-sm-2 control-label hidepola">Email</label>
-                    <div class="col-sm-10">
-                        <input class="form-control" data-error="El correo de seguir esta forma ejemplo.correo@dominio.com" pattern="^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$" type="email" name="email" placeholder="Correo" required>
-                        <div class="help-block with-errors" ></div>
-                    </div>
-                </div>
-                <div class="form-group has-feedback">
-                    <label for="password" class="col-sm-2 control-label hidepola">Contraseña</label>
-                    <div class="col-sm-10">
-                      <input data-minlength-error="Minimo son 6 caracteres" data-minlength="6" maxlength="16" class="form-control" type="password" name="password" placeholder="Contraseña" required>
-                      <div class="help-block with-errors"></div>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-sm-offset-2 col-sm-10 col-xs-12">
-                      <div class="checkbox" name="rememberme">
-                        <label>
-                          <input type="checkbox" name="rememberme"> Recordarme
-                          <a href="{{ URL::route('recoverView') }}">¿Olvidaste tu Contraseña?</a>
-                        </label>
-                      </div>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-sm-offset-2 col-sm-10">
-                      {!! Form::submit('Iniciar Sesion',['class' => 'btn btn-primary']) !!}
-                    </div>
-                </div>
-                {!! Form::close() !!}
+              @yield('loginForm')
           </div>
         </div>
     </div>
