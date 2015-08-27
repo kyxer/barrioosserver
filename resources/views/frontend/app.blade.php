ba75135dc6dbd22c37a8afa8f7e4bf9fe572eb58
German Mendoza <german.mendoza.187@gmail.com>
2015-08-27 18:36:28 -0430

    fix window

--- resources/views/frontend/app.blade.php
+++ resources/views/frontend/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" >
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    @if(isset($dashboard))
+    @if(isset($auth))
         <meta name="viewport" content="width=device-width, initial-scale=1">
     @else
         <meta name="viewport" content="user-scalable=yes">
